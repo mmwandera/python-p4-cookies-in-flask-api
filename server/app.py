ee73452ea7ac131479937aa5e052185cbce4d585
mmwandera <mmwandera74@gmail.com>
2024-03-07 11:23:15 +0300

    completed code along

--- server/app.py
+++ server/app.py
@@ -1,16 +1,20 @@
 from flask import Flask, request, session, jsonify, make_response
 
 app = Flask(__name__)
+# # Disable compact JSON representation in the response
 app.json.compact = False
 
+# # Set a secret key for the Flask application
 app.secret_key = b'?w\x85Z\x08Q\xbdO\xb8\xa9\xb65Kj\xa9_'
 
 @app.route('/sessions/<string:key>', methods=['GET'])
 def show_session(key):
 
+    # Set default values for session variables if not already set
     session["hello"] = session.get("hello") or "World"
     session["goodnight"] = session.get("goodnight") or "Moon"
 
+    # Create a response containing session information and cookies
     response = make_response(jsonify({
         'session': {
             'session_key': key,
@@ -21,6 +25,7 @@ def show_session(key):
             for cookie in request.cookies],
     }), 200)
 
+    # Set a new cookie named 'mouse' with the value 'Cookie'
     response.set_cookie('mouse', 'Cookie')
 
     return response
